2e57ff3fcf2f51c2eafacbaec075957d43b860d0
UmarFarooq361 <umarrje36@gmail.com>
2024-07-28 23:26:37 +0500

    Main file

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,17 @@
 
-// import React from 'react';
+import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
-// import { CssBaseline } from '@mui/material';
+import { CssBaseline } from '@mui/material';
 
 // Create a root for the React app
 const root = createRoot(document.getElementById('root')!);
 
 // Render the App component
 root.render(
-  // <React.StrictMode>
-    // <CssBaseline />
+  <React.StrictMode>
+    <CssBaseline />
     <App />
-  // </React.StrictMode>
+  </React.StrictMode>
 );
 
